b615c75091bcd7834651b07ee47a48126d7d2059
lizhengfu <405574395@qq.com>
2013-07-04 18:43:27 +0800

    init

--- example/app.js
+++ example/app.js
@@ -1,8 +1,18 @@
-var Sinalogin = require('../lib')
+var Sinalogin = require('../lib');
+var request = require('request');
 
 var account = {
 	name: '*@qq.com',
-	passwd: '*'
+	passwd: '*',
+	cookiefile: '*@qq.com.dat'
 }
 
-Sinalogin.weibo_login(account)
+Sinalogin.weibo_login(account, function(err, loginInfo){
+	if(loginInfo.logined){
+		var j = loginInfo.j;
+
+		request({url: 'http://weibo.com/youyudehexie?wvr=5&wvr=5&lf=reg', jar: j}, function (err, response, body) {
+		  console.log(body)
+		});
+	}
+}) 
